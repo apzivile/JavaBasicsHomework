a9b8eaa492ea0ee7e4387ddb72bb5802ea72f6d5
apzivile <39947097+apzivile@users.noreply.github.com>
2018-08-08 11:41:13 +0300

    Update Main.java

--- 0807/payment/Main.java
+++ 0807/payment/Main.java
@@ -8,7 +8,7 @@ public class Main {
         Employee em2= new Employee(15487265 , "Alex" , 2000);
         Client cl1= new Client(41254163 , "Facebook", 15000);
         Client cl2= new Client(41254512 , "Google" , 20000);
-        Payment[] arr = new Payment[12];
+        Object[] arr = new Object[12];
         arr[0] = em1.bankAccount();
         arr[1] = em2.bankAccount();
         arr[2] = cl1.bankAccount();
@@ -27,7 +27,7 @@ public class Main {
         System.out.println(cl1.toString());
         System.out.println(cl2.toString());
 //        System.out.println(Arrays.toString(arr));
-        for(Payment i : arr){
+        for(Object i : arr){
             System.out.println(i);
         }
     }
